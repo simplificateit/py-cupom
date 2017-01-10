88a17bc3467090aa83e1c0aeb1a961cbf7efa07c
AlexVanAxe <alexvanaxe@gmail.com>
2017-01-10 17:33:10 -0200

    Updating tag

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'py_cupom',
-  packages = ['py_cupom'], # this must be the same as the name above
+  packages = ['py_cupom'],
   version = '0.1',
   description = 'A python script to generate temporary codes with low collision rate.',
   author = 'AlexVanAxe',
   author_email = 'alexvanaxe@gmail.com',
-  url = 'https://github.com/simplificateit/py-cupom', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
-  keywords = ['cupom', 'generator', 'random'], # arbitrary keywords
+  url = 'https://github.com/simplificateit/py-cupom',
+  download_url = 'https://github.com/simplificateit/py-cupom/tree/0.1',
+  keywords = ['cupom', 'generator', 'random'],
   classifiers = [],
 )
